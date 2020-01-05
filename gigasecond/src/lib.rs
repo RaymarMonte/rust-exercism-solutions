bdb43daca1d76a87c48ba0e5c87148152b15db83
Raymar Monte <raymarmontecs@gmail.com>
2020-01-05 11:44:46 +0800

    1st iteration of gigasecond problem

---
+++ gigasecond/src/lib.rs
@@ -0,0 +1,7 @@
+use chrono::{DateTime, Utc};
+use time::Duration;
+
+// Returns a Utc DateTime one billion seconds after start.
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    start.checked_add_signed(Duration::seconds(1000000000)).unwrap()
+}
