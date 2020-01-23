5e7920e121f5b8ad9058a7c3e27f80145fd52720
Raymar Monte <raymarmontecs@gmail.com>
2020-01-23 20:49:16 +0800

    Solved bob exercise using regex

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,3 +1,15 @@
+use regex::Regex;
+
 pub fn reply(message: &str) -> &str {
-    
+    match message {
+        x if Regex::new(r"^\s*$").unwrap().is_match(x)
+            => "Fine. Be that way!",                // Whitespace
+        x if Regex::new(r"^[^a-z]*[A-Z][^a-z]*\?\s*$").unwrap().is_match(x)
+            => "Calm down, I know what I'm doing!", // All Caps Question
+        x if Regex::new(r"^[^a-z]*[A-Z][^a-z]*$").unwrap().is_match(x)
+            => "Whoa, chill out!",                  // All Caps
+        x if Regex::new(r"^.*\?\s*$").unwrap().is_match(x)
+            => "Sure.",                             // Normal Question
+        _ => "Whatever.",                           // Random
+    }
 }
---
+++ bob/src/main.rs
@@ -0,0 +1,5 @@
+use bob::reply;
+
+fn main() {
+    println!("{}", reply("Tom-ay-to, tom-aaaah-to."));
+}
