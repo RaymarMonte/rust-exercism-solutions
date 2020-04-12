1398c7993ad86d17ef28a2819811f5f488b9a028
Raymar Monte <raymarmontecs@gmail.com>
2020-04-12 14:26:20 +0800

    Solved saddle-points exercise

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,6 +1,42 @@
+use array_tool::vec::Intersect;
+
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    // Get the highest value positions per row
+    let mut highest_per_row: Vec<(usize, usize)> = Vec::new();
+    for (i, row) in input.iter().enumerate() {
+        let mut highest_val: u64 = 0;
+        let mut highest_pos: Vec<(usize, usize)> = Vec::new();
+        for (j, cell) in row.iter().cloned().enumerate() {
+            if cell > highest_val {
+                highest_val = cell;
+                highest_pos.clear();
+                highest_pos.push((i, j));
+            } else if cell == highest_val {
+                highest_pos.push((i, j));
+            }
+        }
+        highest_per_row.append(&mut highest_pos);
+    }
+    // Get the lowest value positions per column
+    let mut lowest_per_col: Vec<(usize, usize)> = Vec::new();
+    let mut col_count = 0;
+    for row in input.iter().take(1) {
+        col_count = row.len();
+    }
+    for j in 0..col_count {
+        let mut lowest_val: u64 = 10;
+        let mut lowest_pos: Vec<(usize, usize)> = Vec::new();
+        for i in 0..input.len() {
+            if input[i][j] < lowest_val {
+                lowest_val = input[i][j];
+                lowest_pos.clear();
+                lowest_pos.push((i, j));
+            } else if input[i][j] == lowest_val {
+                lowest_pos.push((i, j));
+            }
+        }
+        lowest_per_col.append(&mut lowest_pos);
+    }
+    // Get the intersection of the two vec's
+    highest_per_row.intersect(lowest_per_col)
 }
---
+++ saddle-points/src/main.rs
@@ -0,0 +1,6 @@
+use saddle_points::find_saddle_points;
+
+fn main() {
+    let input = vec![vec![8, 7, 9], vec![6, 7, 6], vec![3, 2, 5]];
+    let _output = find_saddle_points(&input);
+}
