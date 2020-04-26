5016da5bf2b203487b89e705c54e3d7e498b3036
Raymar Monte <raymarmontecs@gmail.com>
2020-04-26 17:52:44 +0800

    Implemented solution for isbn-verifier

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -1,4 +1,25 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    unimplemented!("Is {:?} a valid ISBN number?", isbn);
+    let normalized_isbn = &isbn.trim().replace('-', "");
+    if normalized_isbn.len() != 10 {
+        return false;
+    }
+    let mut validity = 0;
+    // i: index, c: character; enumerate() returns tuple consisting of the
+    // index and the actual data
+    for (i, c) in normalized_isbn.chars().enumerate() {
+        // to_digit() requires the base number. 10 = decimal
+        let digit = match c.to_digit(10) {
+            Some(x) => x,   // If c is indeed a digit
+            None => {       // If not
+                if i == 9 && c == 'X' {
+                    10      // No semicolon means return the value
+                } else {
+                    return false;
+                }
+            }
+        };
+        validity += digit * (10 - (i as u32));
+    }
+    (validity % 11) == 0
 }
---
+++ isbn-verifier/src/main.rs
@@ -0,0 +1,5 @@
+use isbn_verifier::is_valid_isbn;
+
+fn main() {
+    println!("{}", is_valid_isbn("3-598-21508-8"));
+}
