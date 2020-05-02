ff6a3c9ef54fdfe89b6ff01da693b5da55865e1a
Raymar Monte <raymarmontecs@gmail.com>
2020-05-02 15:27:58 +0800

    Implemented solution for perfect-numbers

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -6,5 +6,22 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {}", num);
+    if num == 0 {
+        return None
+    }
+    let mut factors = Vec::new();
+    for i in 1..num {
+        if num % i == 0 {
+            factors.push(i);
+        }
+    }
+    let aliquot_sum: u64 = factors.iter().sum::<u64>();
+    if aliquot_sum == num {
+        return Some(Classification::Perfect);
+    } else if aliquot_sum > num {
+        return Some(Classification::Abundant);
+    } else if aliquot_sum < num {
+        return Some(Classification::Deficient);
+    }
+    None
 }
