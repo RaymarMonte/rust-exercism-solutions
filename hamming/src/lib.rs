20a0e86d7eaa3a81792669d594123544b2510492
Raymar Monte <raymarmontecs@gmail.com>
2020-05-03 17:03:33 +0800

    Added solution for hamming problem

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -1,5 +1,16 @@
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    unimplemented!("What is the Hamming Distance between {} and {}", s1, s2);
+    if s1.len() != s2.len() {
+        return None
+    }
+    let mut s1_chars = s1.chars();
+    let mut s2_chars = s2.chars();
+    let mut distance = 0;
+    for _ in 0..s1.len() {
+        if s1_chars.next() != s2_chars.next() {
+            distance += 1;
+        }
+    }
+    Some(distance)
 }
